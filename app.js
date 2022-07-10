81cc347489f2661793942e2a95e7359c71e830b7
Bhimgouda <satvikp508@gmail.com>
2022-07-10 15:09:33 +0530

    almost completed

--- app.js
+++ app.js
@@ -1,16 +1,125 @@
-async function getIpAddress(){
-    let config = {params:{token:'7f38ccd56f3c4e'}}
-    let ipData = await axios.get('https://ipinfo.io/', config)
-    return ipData.data;
+const userInput = document.querySelector('.search-box input');
+const findIpArrow = document.querySelector('.search-box .icon--arrow');
+const userIp = document.querySelector('#ip-address .value');
+const userLocation = document.querySelector('#location .value');
+const userTimezone = document.querySelector('#timezone .value');
+const userIsp = document.querySelector('#isp .value');
+const ipDisplay = document.querySelector('.ip-display');
+
+
+
+/* Changing map marker Icon of map*/
+const myIcon = L.icon({
+    iconUrl: '/images/icon-location.svg',
+    iconSize: [30,40],
+    iconAnchor: [16,42]
+});
+
+// Initialize map
+let map = L.map('map',{dragging:false}).setView([0,0], 1);
+let tileUrl = `https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`;
+let tileLayer = L.tileLayer(tileUrl, {maxZoom: 19}).addTo(map);
+let marker = L.marker([0,0],{icon:myIcon});
+
+
+
+
+/* On load function */
+window.addEventListener('load', async ()=>{
+    displayIpAddress();
+    locationOnMap();
+});
+
+/* When user Enters a custom Ip address and clicks on arrow btn */
+findIpArrow.addEventListener('click',()=>{
+    let customIp = userInput.value;
+    displayIpAddress(customIp);
+    locationOnMap(customIp);
+})
+
+/* When user Enters a custom Ip address and clicks ENTER on keyboard */
+window.addEventListener('keyup',event=>{
+    if (event.keyCode === 13 && userInput){
+        let customIp = userInput.value;
+        displayIpAddress(customIp);
+        locationOnMap(customIp);
+    }
+})
+
+
+
+
+
+
+
+
+
+/* Fetch Ip address and details from ipinfo */
+async function getIpAddress(ipAddress){
+    try{
+        let config = {params:{token:'7f38ccd56f3c4e'}}
+        if(ipAddress){
+            let ipData = await axios.get(`https://ipinfo.io/${ipAddress}`, config)
+            return ipData.data;
+        }
+        else{
+            let ipData = await axios.get(`https://ipinfo.io/`, config)
+            return ipData.data;
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
 }
 
-getIpAddress()
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+/* To display users fetched IP address on screen */
+async function displayIpAddress(ipAddress){
+    try{
+        let data = await getIpAddress(ipAddress);
+        userIp.textContent = `${data.ip}`;
+        userLocation.textContent = `${data.city}, ${data.region}, ${data.country}`;
+        userTimezone.textContent = `${data.timezone}`;
+        userIsp.textContent = `${data.hostname}`;
+        userInput.value = data.ip;
+    }
+    catch(err){
+        console.log(err)
+    }
+}
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
-}).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+
+/* Changing map tile and marker according to data.json */
+async function locationOnMap(ipAddress){
+    try{
+        let data = {};
+        if(ipAddress){
+            data = await getIpAddress(ipAddress)
+        }
+        else{
+            data = await getIpAddress();
+        }
+        let currentLocation = data.loc.split(',');
+        map.flyTo([`${(currentLocation[0])}`,`${currentLocation[1]}`], 7,{
+            duration:0.7
+        });
+        marker.setLatLng([`${(currentLocation[0])}`,`${currentLocation[1]}`]).addTo(map);
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
